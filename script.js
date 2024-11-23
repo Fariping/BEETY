e500b29019774b9e21926dad14ec8da7164581d4
Fares <144069751+Fariping@users.noreply.github.com>
2024-11-23 22:18:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // تهيئةEmailJS
-emailjs.init("-_n0qbKnDcV32oAOP");
+
 
 //  عند الضغط على زر الإرسال
 document.getElementById("submit-order").addEventListener("click", function () {
@@ -39,7 +39,8 @@ document.getElementById("submit-order").addEventListener("click", function () {
         sideDish2: sideDish2,
     };
 
-    // إرسال البريد باستخدام EmailJS
+    emailjs.init("-_n0qbKnDcV32oAOP");
+// إرسال البريد باستخدام EmailJS
     emailjs
         .send("service_t9ogwct", "template_default", templateParams)
         .then(function (response) {
